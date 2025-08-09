f45a140475a27de97751a1b7464e40449bff1b9e
Minh Tran <trankhaiminh0911@gmail.com>
2025-08-09 11:27:04 +0700

    Update blink config

--- neovim/.config/nvim/lua/plugins/blink.lua
+++ neovim/.config/nvim/lua/plugins/blink.lua
@@ -1,119 +1,153 @@
 return {
-  'saghen/blink.cmp',
-  enabled = true,
+  -- add blink.compat
+  {
+    'saghen/blink.compat',
+    -- use v2.* for blink.cmp v1.*
+    version = '2.*',
+    -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+    lazy = true,
+    -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+    opts = {},
+  },
 
-  -- optional: provides snippets for the snippet source
-  dependencies = { 'rafamadriz/friendly-snippets' },
+  {
+    'saghen/blink.cmp',
+    enabled = true,
 
-  -- use a release tag to download pre-built binaries
-  version = '1.*',
-  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  -- build = 'cargo build --release',
-  -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
-
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
-  opts = {
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = {
-      ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-      ['<C-e>'] = { 'cancel', 'fallback' },
-      ['<Tab>'] = {'select_next', 'snippet_forward', 'fallback',},
-      ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
-
-      ['<CR>'] = { 'accept', 'fallback' },
-
-      ['<Up>'] = { 'select_prev', 'fallback' },
-      ['<Down>'] = { 'select_next', 'fallback' },
-      ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
-      ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
-
-      ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-      ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-
-      ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+    -- optional: provides snippets for the snippet source
+    dependencies = {
+      'L3MON4D3/LuaSnip',
+      'rafamadriz/friendly-snippets',
+      'bydlw98/blink-cmp-env',
+      'hrsh7th/cmp-nvim-lua'
     },
 
-    appearance = {
-      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'mono'
-    },
+    -- use a release tag to download pre-built binaries
+    version = '1.*',
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
 
-    -- (Default) Only show the documentation popup when manually triggered
-    completion = {
-      documentation = { auto_show = false },
-      list = {
-        selection = {
-          preselect = false,
-        }
-      },
-      menu = {
-        draw = { columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } } },
-        -- border = 'rounded',
-      },
-      -- documentation = { window = { border = 'rounded' } },
-    },
-
-    signature = {
-      enabled = true,
-    --   window = { border = 'rounded' }
-    },
-
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-      providers = {
-        lsp = { min_keyword_length = 3 },
-        path = {
-          opts = {
-            trailing_slash = true,
-            label_trailing_slash = true,
-            show_hidden_files_by_default = true
-          }
-        },
-        snippets = { min_keyword_length = 5 },
-        buffer = { min_keyword_length = 5 },
-      }
-    },
-
-    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-    --
-    -- See the fuzzy documentation for more information
-    fuzzy = { implementation = "rust" },
-
-    cmdline = {
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
       keymap = {
-        preset = 'inherit',
-        ['<Tab>'] = { 'select_next', 'fallback' },
-        ['<S-Tab>'] = { 'select_prev', 'fallback' }
+        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-e>'] = { 'cancel', 'fallback' },
+        ['<Tab>'] = {'select_next', 'snippet_forward', 'fallback',},
+        ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
+
+        ['<CR>'] = { 'accept', 'fallback' },
+
+        ['<Up>'] = { 'select_prev', 'fallback' },
+        ['<Down>'] = { 'select_next', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+        ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+        ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
       },
+
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
+      },
+
+      -- (Default) Only show the documentation popup when manually triggered
       completion = {
-        menu = { auto_show = true },
-        ghost_text = { enabled = true },
+        documentation = { auto_show = false },
         list = {
           selection = {
             preselect = false,
           }
+        },
+        menu = {
+          draw = { columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } } },
+          -- border = 'rounded',
+        },
+        -- documentation = { window = { border = 'rounded' } },
+      },
+
+      signature = {
+        enabled = true,
+      --   window = { border = 'rounded' }
+      },
+
+      snippets = { preset = 'luasnip' },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { 'lsp', 'path', 'nvim_lua', 'snippets', 'buffer',  'env' },
+        providers = {
+          lsp = { min_keyword_length = 3 },
+          path = {
+            opts = {
+              trailing_slash = true,
+              label_trailing_slash = true,
+              show_hidden_files_by_default = true
+            }
+          },
+          snippets = { min_keyword_length = 5 },
+          buffer = { min_keyword_length = 5 },
+          env = {
+            name = "Env",
+            module = "blink-cmp-env",
+            --- @type blink-cmp-env.Options
+            opts = {
+              show_braces = true,
+              show_documentation_window = true,
+            },
+          },
+          nvim_lua = {
+            -- IMPORTANT: use the same name as you would for nvim-cmp
+            name = 'nvim_lua',
+            module = 'blink.compat.source',
+          }
         }
       },
-    },
 
-  },
-  opts_extend = { "sources.default" }
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = "rust" },
+
+      cmdline = {
+        keymap = {
+          preset = 'inherit',
+          ['<Tab>'] = { 'select_next', 'fallback' },
+          ['<S-Tab>'] = { 'select_prev', 'fallback' }
+        },
+        completion = {
+          menu = { auto_show = true },
+          ghost_text = { enabled = true },
+          list = {
+            selection = {
+              preselect = false,
+            }
+          }
+        },
+      },
+
+    },
+    opts_extend = { "sources.default" }
+  }
 }
